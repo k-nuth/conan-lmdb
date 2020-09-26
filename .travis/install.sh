2636b63a77ed8ec9af655b5e22f618cb39a4edf4
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-09-25 21:14:26 -0300

    support for xcode12

--- .travis/install.sh
+++ .travis/install.sh
@@ -19,7 +19,7 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     pyenv activate conan
 fi
 
-pip install conan_package_tools==0.32.4 > /dev/null
+pip install conan_package_tools==0.34.2 > /dev/null
 # pip install kthbuild==0.0.14 > /dev/null
 pip install kthbuild --upgrade > /dev/null 
 
